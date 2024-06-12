03c30091749a66a4c7009f1648c6e9ef253ac057
francescosalzano1 <francesco.salzanosf@gmail.com>
2024-06-12 12:02:19 +0200

    syncronized csvs and tagged slither vulnerabilties

---
+++ smartbugs-results/metadata/syncronize.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+# Read the CSV files
+csv1 = pd.read_csv('vulnerabilities_mapping_rev1.csv', engine="python")
+csv2 = pd.read_csv('vulnerabilities_mapping_rev2.csv')
+
+# Filter rows where Tools is 'slither' in csv1
+slither_rows_csv1 = csv1[csv1['Tools'] == 'slither']
+
+# Check for descriptions in csv2
+existing_descriptions_csv2 = csv2[csv2['Tools'] == 'slither']['Vulnerability name'].tolist()
+
+# Find rows in csv1 where description is not in csv2
+new_rows = slither_rows_csv1[~slither_rows_csv1['Vulnerability name'].isin(existing_descriptions_csv2)]
+
+# Set other columns to null and Tools to 'slither'
+new_rows.loc[:, new_rows.columns != 'Vulnerability name'] = None
+new_rows['Tools'] = 'slither'
+
+# Concatenate the new rows to csv2
+csv2 = pd.concat([csv2, new_rows], ignore_index=True)
+
+# Save the updated csv2
+csv2.to_csv('path_to_updated_csv2.csv', index=False)
